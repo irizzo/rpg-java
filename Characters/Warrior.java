fd85f7ab23d9cc7f5993de9c6fd4b09017519d97
Isabelle Rizzo <isabelle965rizzo@gmail.com>
2021-10-22 20:52:52 -0300

    warrior's special ability

--- Characters/Warrior.java
+++ Characters/Warrior.java
@@ -1,9 +1,17 @@
 package Characters;
 
+import Dice.Dice;
+
 public class Warrior extends RpgCharacter {
+  Dice d6 = new Dice(6);
+
   public Warrior(String warriorName, int warriorHp, int warriorStrength, int warriorDefense) {
     super(warriorName, warriorHp, warriorStrength, warriorDefense);
   };
 
   // special ability: rage
+  public void rage(RpgCharacter enemy) {
+    int damage = d6.roll() + 1;
+    enemy.hp -= damage;
+  }
 };
