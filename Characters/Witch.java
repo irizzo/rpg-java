8ac2ef8f1adb98b1228df75ad3a4bad445dadb08
Isabelle Rizzo <isabelle965rizzo@gmail.com>
2021-10-22 20:53:04 -0300

    witch's special ability

--- Characters/Witch.java
+++ Characters/Witch.java
@@ -1,11 +1,18 @@
 package Characters;
 
+import Dice.Dice;
+
 public class Witch extends RpgCharacter {
+  Dice d6 = new Dice(6);
+
   public Witch(String witchName, int witchHp, int witchStrength, int witchDefense) {
     super(witchName, witchHp, witchStrength, witchDefense);
   };
 
   // special ability: poisioning
-  // public static void poison() {
-  // };
+  public void poison(RpgCharacter enemy) {
+    int damage = d6.roll() + 2;
+
+    enemy.hp -= damage;
+  };
 };
