fdc6376f33055c40dc1ef2f35fcd4ec6ae8f152e
Isabelle Rizzo <isabelle965rizzo@gmail.com>
2021-11-13 12:04:10 -0300

    actions package

---
+++ Actions/Battle1V1.java
@@ -0,0 +1,16 @@
+package Actions;
+
+// import java.util.ArrayList;
+
+// import Characters.RpgCharacter;
+
+public class Battle1V1 {
+  // each 1v1 battle will have:
+  // 2 players
+  // 1 scenarioType (where is the battle happening)
+  // 1 result (1 winner or tie)
+
+  // public ArrayList<RpgCharacter> playerList = new ArrayList<RpgCharacter>();
+  // public Scenario scenarioType = new Scenario
+  // winner
+}
---
+++ Actions/actions.md
@@ -0,0 +1,10 @@
+# Actions Package :construction:
+
+## PT-BR
+<!-- As ações são as interações entre os personagens. Por exemplo:
+  Batalha 1v1, onde dois personagens se atacam (um round), e dependendo das características da batalha os personagens podem usar habilidades especiais -->
+
+---
+
+## EN
+
